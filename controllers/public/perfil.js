25421e34f7263fe0c332a7ed69fdd306fa948170
Joel <joelomarmena@gmail.com>
2024-06-18 18:57:50 -0600

    pantallas de diseño hechas

--- controllers/public/perfil.js
+++ controllers/public/perfil.js
@@ -1,6 +1,13 @@
-
-
-
+// Constantes para establecer los elementos del componente Modal.
+const SAVE_MODAL = new bootstrap.Modal('#saveModal'),
+    MODAL_TITLE = document.getElementById('modalTitle');
+// Constantes para establecer los elementos del formulario de guardar.
+const SAVE_FORM = document.getElementById('saveForm'),
+    ID_PERFIL = document.getElementById('idPerfil'),
+    NOMBRE_PERFIL = document.getElementById('nombrePerfil'),
+    APELLIDO_PERFIL = document.getElementById('apellidoPerfil'),
+    CORREO_PERFIL = document.getElementById('correoPerfil'),
+    FECHA_PERFIL = document.getElementById('fechanacimientoPerfil');
 
 document.addEventListener('DOMContentLoaded', async () => {
     // Se cambia el color del apartado donde se encuentra el usuario y se restablecen los otros colores.
@@ -8,3 +15,12 @@ document.addEventListener('DOMContentLoaded', async () => {
     // Llamada a la función para validar sesiones activas.
     cargarPlantilla();
 });
+
+
+const openEdit = () => {
+    // Se muestra la caja de diálogo con su título.
+    SAVE_MODAL.show();
+    MODAL_TITLE.textContent = 'Editar perfil';
+    // Se prepara el formulario.
+    SAVE_FORM.reset();
+}
