d1a183202c00e1400a4f56d9b0f9964b16b33d00
jafet <20220188@ricaldone.edu.sv>
2024-06-26 17:16:00 -0600

    cambios a la program

---
+++ api/models/data/grado_academicos_data.php
@@ -0,0 +1,48 @@
+<?php
+// Se incluye la clase para validar los datos de entrada.
+require_once('../../helpers/validator.php');
+// Se incluye la clase padre.
+require_once('../../models/handlers/grado_academicos_handler.php');
+
+/*
+ *  Clase para manejar el encapsulamiento de los datos de la tabla ADMINISTRADOR.
+ */
+class AspirantesData extends AspirantesHandler
+{
+    // Atributo para el manejo de errores.
+    private $info_error = null;
+
+    /*
+    *  Métodos para validar y asignar los valores de los atributos.
+    */
+    public function setId($valor)
+    {
+        if(Validator::validateNaturalNumber($valor)){
+            $this->id = $valor;
+            return true;
+        }
+        else{
+            $this->info_error = 'El identificador del administrador es correcto';
+            return false;
+        }
+    }
+
+    public function setNombre($valor, $min = 4, $max = 20)
+    {
+        if (!Validator::validateAlphabetic($valor)) {
+            $this->info_error = 'El nombre debe ser un valor alfabético';
+            return false;
+        } elseif (Validator::validateLength($valor, $min, $max)) {
+            $this->nombre = $valor;
+            return true;
+        } else {
+            $this->info_error = 'El nombre debe tener una longitud entre ' . $min . ' y ' . $max;
+            return false;
+        }
+    }
+
+    public function getDataError()
+    {
+        return $this->info_error;
+    }
+}
