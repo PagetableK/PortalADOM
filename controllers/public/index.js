dd66ff2cee6cbef0b6ed483fd652ddc99f8ad201
PagetableK <pablo14gamerx3@gmail.com>
2024-06-25 22:51:11 -0600

    Corrección de validación de sesión sitio privado y público, inicio de importación de interfaces de Villegas

--- controllers/public/index.js
+++ controllers/public/index.js
@@ -3,12 +3,4 @@
 document.addEventListener('DOMContentLoaded', async () => {
     // Llamada a la función para validar sesiones activas.
     cargarPlantilla();
-    // Petición para consultar el correo y el id del aspirante.
-    const DATA = await fetchData(USER_API, 'getUser');
-    // Se comprueba si existe una sesión, de lo contrario se requiere iniciar sesión.
-    if(DATA.session){
-        // Se direcciona a la página web de bienvenida.
-        location.href = 'inicio.html';
-    } else{
-    }
 });
